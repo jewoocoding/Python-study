52ae77324bb31e8758365c440aaec5e6ca09ba2e
jewoo <jewoocode@gmail.com>
2025-08-03 13:05:07 +0900

    ADD: 단계별 심화1 문제풀이

---
+++ 8월/08.03/05.py
@@ -0,0 +1,31 @@
+# 문제
+# 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+
+# 입력
+# 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+
+# 출력
+# 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+
+s = input().upper()
+
+a = []
+for i in range(26) :
+    a.append(0)
+
+for i in range(len(s)) :
+    a[ord(s[i])-65] += 1
+
+maxC = ''
+maxN = 0
+for i in range(26) :
+    if a[i] > maxN :
+        maxC = chr(i+65)
+        maxN = a[i]
+    elif a[i] == maxN and a[i] != 0 :
+        maxC += chr(i+65)
+    
+if len(maxC) == 1:
+    print(maxC)
+else :
+    print('?')
